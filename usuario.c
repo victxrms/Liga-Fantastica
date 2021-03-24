70d82987149459f06dcad0694d2609c1490a090d
Victor Moreno <vmoso2002@gmail.com>
2021-03-24 10:57:51 +0100

    Añadido inicio de sesion

---
+++ usuario.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "usuario.h"
+
+#define SEPARADOR "-------------------------------------\n"
+
+int reg;
+
+int main (){
+  printf ("Bienvenido a la Liga Fantastica\n");
+  printf ("%s", SEPARADOR);
+  do{
+    printf ("Iniciar sesion (1) | Registrarse (2)\n");
+    scanf ("%d", &reg);
+  } while(reg!=1 & reg !=2);
+  if (reg=1){
+    printf ("Introduce usuario: ");
+    scanf ("%c", &nombre_usuario);
+    printf("Introduce contraseña: ");
+    scanf ("%c", &contrasena);
+}
+}

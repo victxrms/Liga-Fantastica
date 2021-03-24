0a746e9687b5fd8db4d20644c3032eee6793f90b
Victor Moreno <vmoso2002@gmail.com>
2021-03-24 15:40:00 +0100

    Corregido

--- usuario.c
+++ usuario.c
@@ -4,7 +4,6 @@
 #include <assert.h>
 #include "usuario.h"
 
-#define SEPARADOR "\t\t------------------------------------\n"
 #define USER 1
 #define CHRONIST 2
 #define ADMIN 3
@@ -22,7 +21,7 @@ int main (void){
         assert(fp != NULL);
 
         printf ("\t\t   Bienvenido a la Liga Fantastica\n");            // Printea el titulo
-        printf ("%s", SEPARADOR);
+        printf ("\t\t------------------------------------\n");
 
         do{
                 printf ("\t\tIniciar sesion (1) | Registrarse (2)\n");  // Pantalla de inicio sesion o registro hasta que se seleccione 1 o 2
@@ -30,19 +29,22 @@ int main (void){
         } while(reg!=1 & reg !=2);
 
         if (reg=1) {
-                printf ("Introduce tu usuario: ");                     // Introduce usuario y comprueba si existe, si es asi introduce contraseña y comprueba contraseña.
-                scanf ("%s", &userinp);                                // Si alguno de los dos es erroneo muestra mensaje de fallo
-                do {
-                        fscanf (fp, "%s, %s", &userarch, &contrarch);
-                        val1 = strcmp(userinp, userarch);
-                        if (val1=0) {
-                                printf ("Introduce tu contraseña: ");
-                                val2 = strcmp(contrinp, contrarch);
-                                if (val2=0) {
-                                        printf ("Inicio de sesion correcto\n");
-                                }else printf("Contraseña incorrecta");
-                        }else printf ("El usuario no existe\n");
-                }while(fgets(currentline, sizeof(currentline), fp) != NULL);
+
+            printf ("Introduce tu usuario: ");                     // Introduce usuario y comprueba si existe, si es asi introduce contraseña y comprueba contraseña.
+            scanf ("%s", &userinp);                                // Si alguno de los dos es erroneo muestra mensaje de fallo
+            do {
+                    fscanf (fp, "%s, %s", &userarch, &contrarch);
+                    val1 = strcmp(userinp, userarch);
+                    if (val1=0) {
+                            printf ("Introduce tu contraseña: ");
+                            val2 = strcmp(contrinp, contrarch);
+                            if (val2=0) {
+                                    printf ("Inicio de sesion correcto\n");
+                            }else printf("Contraseña incorrecta");
+                    }else printf ("El usuario no existe\n");
+
+            }while(fgets(currentline, sizeof(currentline), fp) != NULL);
+
 
 
         }

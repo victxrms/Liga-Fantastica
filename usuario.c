e9ba97b2eb6587a110ca0e377696a322b89fa006
Victor Moreno <vmoso2002@gmail.com>
2021-03-24 13:11:54 +0100

    - Seccion de login de usuario en proceso

--- usuario.c
+++ usuario.c
@@ -1,23 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 #include "usuario.h"
 
-#define SEPARADOR "-------------------------------------\n"
+#define SEPARADOR "\t\t------------------------------------\n"
+#define USER 1
+#define CHRONIST 2
+#define ADMIN 3
 
-int reg;
 
-int main (){
-  printf ("Bienvenido a la Liga Fantastica\n");
+
+int main (void){
+  int reg, i=0;
+
+  char userinp, contrinp, userarch, contrarch, currentline [100];
+
+  FILE* fp;
+
+  assert(fp != NULL);
+
+
+
+  printf ("\t\t   Bienvenido a la Liga Fantastica\n");
   printf ("%s", SEPARADOR);
+
   do{
-    printf ("Iniciar sesion (1) | Registrarse (2)\n");
+    printf ("\t\tIniciar sesion (1) | Registrarse (2)\n");
     scanf ("%d", &reg);
   } while(reg!=1 & reg !=2);
+
   if (reg=1){
-    printf ("Introduce usuario: ");
-    scanf ("%c", &nombre_usuario);
-    printf("Introduce contraseña: ");
-    scanf ("%c", &contrasena);
-}
+    printf ("Introduce tu usuario: ");
+    scanf ("%s", &userinp);
+    while (fgets(currentline, sizeof(currentline), fp) != NULL){
+      fscanf (fp, "%s, %s", userarch, contrarch);
+      printf("%s\n", userarch );
+      printf("%s\n", contrarch );
+    }
+
+  }
+
+
+
+
 }

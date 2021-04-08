07efffbf6ce64a0cb893ade1a826dd96f8618537
elcarlillo <carlos.vidalro@alum.uca.es>
2021-04-08 18:03:45 +0200

    cambio carga.h y nueva funcion de carga

--- carga.h
+++ carga.h
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "configuracion.h"
 #include "equipo.h"
 #include "futbolista.h"
@@ -5,3 +8,4 @@
 void carga_equipos();
 int contar_lineas(FILE *);
 void limpiar(char *);
+void carga_configuracion();

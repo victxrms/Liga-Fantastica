dae07b7ee78b1f99fc0204b6322ff10de5bb3489
Victor Moreno <vmoso2002@gmail.com>
2021-04-08 16:36:02 +0200

    cambio definicion de perfil_usuario y nombre variable global

--- usuario.h
+++ usuario.h
@@ -2,7 +2,7 @@ typedef struct{
 	int identificador_usuario;
 	int numero_total;
 	char nombre_usuario[21];
-	char perfil_usuario[2];
+	int perfil_usuario;
 	char usuario[9];
 	char contrasena[9];
-}usuario;
+}usuarios;

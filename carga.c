0cc90818da8f45f65bcd114ed92621d773971f02
elcarlillo <carlos.vidalro@alum.uca.es>
2021-04-08 18:04:32 +0200

    finalizacion de carga de equipos y carga de configuracion

--- carga.c
+++ carga.c
@@ -1,15 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "carga.h"
 
-FILE *f_equipos;
+FILE *f_equipos, *f_configuracion;
 equipo *total_equipos;
+configuracion *toda_config;
 
 void limpiar(char *string)
 {
   int i;
   char c;
-for(i=0;string[i]!='\0');i++)
+for(i=0;string[i]!='\0';i++)
 {
     string[i]=' ';
 }
@@ -21,7 +20,7 @@ int contar_lineas(FILE *f)
   int cuenta=0;
   while(!feof(f))
 {
-  c=getc(f)
+  c=getc(f);
   if(c=='\n')
 {
   cuenta++;
@@ -33,17 +32,17 @@ int contar_lineas(FILE *f)
 
 void carga_equipos()
 {
-  int numero_equipos, i, j, num=0;
+  int numero_equipos, i, j;
   char letra, cad_aux[21];
   f_equipos=fopen("equipos.txt", "r");
-  if(f_eq==NULL)
+  if(f_equipos==NULL)
 {
   printf("Se ha producido un error al abrir el fichero");
 }
   else
 {
-  numero_equipos=contar_lineas(f_equipos)
-  rewinf(f_equipos);
+  numero_equipos=contar_lineas(f_equipos);
+  rewind(f_equipos);
   total_equipos=(equipo *)malloc(numero_equipos*sizeof(equipo));
   if(total_equipos==NULL)
 {
@@ -52,21 +51,74 @@ void carga_equipos()
   else
 {
   for(i=0;!feof(f_equipos);i++)
-}
-  limpiar(cad_aux)
-  for(j=0;!feof(f_equipos)&& letra!='-';j++)
+{
+  limpiar(cad_aux);
+  for(j=0;!feof(f_equipos) && letra!='-';j++)
 {
   letra=fgetc(f_equipos);
     if(letra!='-')
 {
-    num*=10;
-    num+=atoi(letra);
+  cad_aux[j]=letra;
 }
 }
-  equipo[i].identificador_equipo=num;
-  num=0;
-  for(j=0;!feof(f_equipos) && letra!='\n')
+  total_equipos[i].identificador_equipo=atoi(cad_aux);
+  for(j=0;!feof(f_equipos) && letra!='\n';j++)
+{
+  letra=fgetc(f_equipos);
+  if(letra!='\n')
+{
+  cad_aux[j]=letra;
 }
+}
+  strcpy (total_equipos[i].nombre_equipo, cad_aux);
+}
+}
+}
+}
+
+
+void carga_configuracion()
+{
+  int numero_config, i, j;
+  char letra, cad_aux[31];
+  f_configuracion=fopen("configuracion.txt", "r");
+  if(f_configuracion==NULL)
+{
+  printf("Se ha producido un error al abrir el fichero");
+}
+  else
+{
+  numero_config=contar_lineas(f_configuracion);
+  rewind(f_configuracion);
+  toda_config=(configuracion *)malloc(numero_config*sizeof(configuracion));
+  if(toda_config==NULL)
+{
+  printf("error al reservar memoria");
+}
+  else
+{
+  for(i=0;!feof(f_configuracion);i++)
+{
+  for(j=0;!feof(f_configuracion) && letra!='-';j++)
+{
+  letra=fgetc(f_configuracion);
+  if(letra!='-')
+{
+  cad_aux[j]=letra;
+}
+}
+  strcpy(toda_config[i].campo_configuracion, cad_aux);
+  limpiar(cad_aux);
+  letra='0';
+  for(j=0;!feof(f_configuracion) && letra!='\n';j++)
+{
+  letra=fgetc(f_configuracion);
+  if(letra!='\n')
+{
+  cad_aux[j]=letra;
+}
+}
+  toda_config[i].valor_campo=atoi(cad_aux);
 }
 }
 }

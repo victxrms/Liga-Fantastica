d39aca5c064a75c8de42cdb896ddd5bc1a190c74
victor <vmoso2002@gmail.com>
2021-04-11 20:47:29 +0200

    Añadidas funciones de carga y usuario para redireccionar al resto

--- main.c
+++ main.c
@@ -1,3 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "carga.h"
+
+void usuario ();
+int carga ();
+
+int main (){
+
+  int carga ();
+  void usuario ();
+
+  printf ("\t\tGracias por jugar Liga Fantastica\n");
+
+
+
+}

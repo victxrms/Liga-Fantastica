1366c7ccb8bf1a4884a4c576a7a3d3c480c2fc68
elcarlillo <carlos.vidalro@alum.uca.es>
2021-04-15 17:35:01 +0200

    adicion de carga usuario

--- carga.c
+++ carga.c
@@ -1,8 +1,9 @@
 #include "carga.h"
 
-FILE *f_equipos, *f_configuracion;
+FILE *f_equipos, *f_configuracion, *f_usuario;
 equipo *total_equipos;
 configuraciones *toda_config;
+usuarios *total_usuarios;
 
 void limpiar(char *string)
 {
@@ -74,6 +75,7 @@ void carga_equipos()
                         }
                 }
         }
+        fclose(f_equipos);
 }
 
 
@@ -124,4 +126,89 @@ void carga_configuracion()
                         }
                 }
         }
+        fclose(f_configuracion);
+}
+
+
+void carga_usuario()
+{
+  int n_usuarios, i, j;
+  char cad_n_usuario[21], cad_contra[9], cad_usuario[6], letra;
+  f_usuario=fopen("usuarios.txt", "r");
+  if(f_usuario==NULL)
+{
+  printf("error al arbir fichero usuarios \n");
+}
+  else
+{
+  n_usuarios=contar_lineas(f_usuario);
+  total_usuarios=(usuarios *) malloc (n_usuarios*sizeof(usuarios));
+  if(total_usuarios==NULL)
+{
+  printf("error al reservar memoria");
+}
+  else
+{
+  for(i=0; !feof(f_usuario);i++)
+{
+  limpiar(cad_n_usuario);
+  limpiar(cad_contra);
+  letra='0';
+  for(j=0;letra!='-' && !feof(f_usuario);j++)
+{
+  letra=fgetc(f_usuario);
+  if(letra!='-')
+{
+  cad_usuario[j]=letra;
+}
+}
+  total_usuarios[i].identificador_usuario=atoi(cad_usuario);
+  limpiar(cad_usuario);
+  letra='0';
+  for(j=0;letra!='-'&& !feof(f_usuario);j++)
+{
+  letra=fgetc(f_usuario);
+  if(letra!='-')
+{
+  cad_n_usuario[j]=letra;
+}
+}
+  strcpy(total_usuarios[i].nombre_usuario, cad_n_usuario);
+  limpiar(cad_n_usuario);
+  letra='0';
+  for(j=0;letra!='-' && !feof(f_usuario);j++)
+{
+  letra=fgetc(f_usuario);
+  if (letra!='-')
+{
+  cad_usuario[j]=letra;
+}
+}
+  total_usuarios[i].perfil_usuario=atoi(cad_usuario);
+  limpiar(cad_usuario);
+  letra='0';
+for(j=0;letra!='-' && !feof(f_usuario);j++)
+{
+  letra=fgetc(f_usuario);
+  if(letra!='-')
+{
+  cad_usuario[j]=letra;
+}
+}
+  strcpy(total_usuarios[i].user, cad_usuario);
+  limpiar(cad_usuario);
+  letra='0';
+  for(j=0;letra!='\n' && !feof(f_usuario);j++)
+{
+  letra=fgetc(f_usuario);
+  if(letra!='\n')
+{
+  cad_contra[j]=letra;
+}
+}
+  strcpy(total_usuarios[i].contrasena, cad_contra);
+}
+}
+}
+  fclose(f_usuario);
 }

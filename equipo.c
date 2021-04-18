5f1388d19453fbc295ef01ea9ad8a4664d8796e9
AlbertoValderas <albertovg2512@gmail.com>
2021-04-18 21:50:59 +0200

    introduccion de jugadores

--- equipo.c
+++ equipo.c
@@ -40,7 +40,7 @@ void printinpos(int x, char texto[50])
     int fin=0;
     char letra;
 
-    for(int i=-1; i<x, i++)
+    for(int i=-1, i<x, i++)
     {
         letra=texto[i];
 
@@ -56,9 +56,9 @@ void printinpos(int x, char texto[50])
 
 
 
-int main()
+void equipo()
 {
-    int i, opt, codigo;
+    int i, opt, codigo[10];
     char menu1, name[25], string[50];
 
     do
@@ -98,13 +98,70 @@ int main()
                         opt=1;
 
                     if(opt!=1)
-                        printinpos(40, string);
+                        printinpos(40, string[]);
                 }while(opt!=1);
 
                 rewind(jugadores);
 
-                printf("\n\nIntroduzca el codigo de su %s -->", &posicion);
-                scanf("%i", &codigo);
+                printf("\n\nIntroduzca el codigo de su portero -->");
+
+                do
+                {
+                    scanf("%i", &codigo[0]);
+                    if(codigo[0]%4=1&&codigo[0]<=80)
+                    {
+                        //escribir en txt
+                    }
+                    else
+                        printf("El jugador no existe o no es un portero.\n\nVuelva a introducir el codigo -->");
+                }while(codigo[0]%4!=1||codigo[0]>80);
+
+                printf("\n\nIntroduzca el codigo de sus cuatro defensas (separados por espacios) -->");
+
+                do
+                {
+                    scanf("%i%i%i%i", &codigo[1], &codigo[2], &codigo[3], &codigo[4]);
+                    if(codigo[1]%4=2&&codigo[1]<=80&&codigo[2]%4=2&&codigo[2]<=80&&codigo[3]%4=2&&codigo[3]<=80&&codigo[4]%4=2&&codigo[4]<=80)
+                    {
+                        //escribir en txt
+                    }
+                    else
+                        printf("Uno/varios de los jugadores no existen o no son un defensa.");
+                }while(codigo[1]%4!=2||codigo[1]>80||codigo[2]%4!=2||codigo[2]>80||codigo[3]%4!=2||codigo[3]>80||codigo[4]%4!=2||codigo[4]>80);
+
+                printf("\n\nIntroduzca el codigo de sus tres centrocampistas (separados por espacios) -->");
+
+                do
+                {
+                    scanf("%i%i%i", &codigo[5], &codigo[6], &codigo[7]);
+                    if(codigo[5]%4=2&&codigo[5]<=80&&codigo[6]%4=2&&codigo[6]<=80&&codigo[7]%4=2&&codigo[7]<=80)
+                    {
+                        //escribir en txt
+                    }
+                    else
+                        printf("Uno/varios de los jugadores no existen o no son un defensa.");
+                }while(codigo[5]%4!=2||codigo[5]>80||codigo[6]%4!=2||codigo[6]>80||codigo[7]%4!=2||codigo[7]>80);
+
+                printf("\n\nIntroduzca el codigo de sus tres delanteros (separados por espacios) -->");
+
+                do
+                {
+                    scanf("%i%i%i", &codigo[8], &codigo[9], &codigo[10]);
+                    if(codigo[8]%4=2&&codigo[8]<=80&&codigo[9]%4=2&&codigo[9]<=80&&codigo[10]%4=2&&codigo[10]<=80)
+                    {
+                        //escribir en txt
+                    }
+                    else
+                        printf("Uno/varios de los jugadores no existen o no son un defensa.");
+                }while(codigo[8]%4!=2||codigo[8]>80||codigo[9]%4!=2||codigo[9]>80||codigo[10]%4!=2||codigo[10]>80);
+
+                printf("Sus jugadores son:");
+
+                for(i=0,i<=10,i++)
+                {
+                    fgets()
+                }
+
             }
         break;
     }

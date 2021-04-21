f4c1261d355616f7a52f716ab384dc96a834b3e2
elcarlillo <carlos.vidalro@alum.uca.es>
2021-04-22 01:57:55 +0200

    final carga.c

--- carga.c
+++ carga.c
@@ -1,12 +1,6 @@
 #include "carga.h"
-
 FILE *f_equipos, *f_configuracion, *f_usuario, *f_futbolista, *f_plantillas, *f_jugadores_plantilla;
-equipo *total_equipos;
-configuraciones *toda_config;
-usuarios *total_usuarios;
-futbolista *total_futbolistas;
-plantilla *total_plantillas;
-jugadores_plantilla *total_jugad_plant;
+
 
 void limpiar(char *string)
 {
@@ -35,7 +29,7 @@ int contar_lineas(FILE *f)
 
 void carga_equipos()
 {
-        int numero_equipos, i, j;
+        int i, j;
         char letra, cad_aux[21];
         f_equipos=fopen("equipos.txt", "r");
         if(f_equipos==NULL)
@@ -44,9 +38,9 @@ void carga_equipos()
         }
         else
         {
-                numero_equipos=contar_lineas(f_equipos);
+                lineas_equipos=contar_lineas(f_equipos);
                 rewind(f_equipos);
-                total_equipos=(equipo *)malloc(numero_equipos*sizeof(equipo));
+                total_equipos=(equipo *)malloc(lineas_equipos*sizeof(equipo));
                 if(total_equipos==NULL)
                 {
                         printf("error al reservar memoria \n");
@@ -83,7 +77,7 @@ void carga_equipos()
 
 void carga_configuracion()
 {
-        int numero_config, i, j;
+        int i, j;
         char letra, cad_aux[31];
         f_configuracion=fopen("configuracion.txt", "r");
         if(f_configuracion==NULL)
@@ -92,9 +86,9 @@ void carga_configuracion()
         }
         else
         {
-                numero_config=contar_lineas(f_configuracion);
+                lineas_config=contar_lineas(f_configuracion);
                 rewind(f_configuracion);
-                toda_config=(configuraciones *)malloc(numero_config*sizeof(configuraciones));
+                toda_config=(configuraciones *)malloc(lineas_config*sizeof(configuraciones));
                 if(toda_config==NULL)
                 {
                         printf("error al reservar memoria");
@@ -134,7 +128,7 @@ void carga_configuracion()
 
 void carga_usuario()
 {
-        int n_usuarios, i, j;
+        int i, j;
         char cad_n_usuario[21], cad_contra[9], cad_usuario[6], letra;
         f_usuario=fopen("usuarios.txt", "r");
         if(f_usuario==NULL)
@@ -143,9 +137,9 @@ void carga_usuario()
         }
         else
         {
-                n_usuarios=contar_lineas(f_usuario);
+                lineas_usuarios=contar_lineas(f_usuario);
                 rewind(f_usuario);
-                total_usuarios=(usuarios *) malloc (n_usuarios*sizeof(usuarios));
+                total_usuarios=(usuarios *) malloc (lineas_usuarios*sizeof(usuarios));
                 if(total_usuarios==NULL)
                 {
                         printf("error al reservar memoria");
@@ -219,7 +213,7 @@ void carga_usuario()
 
 void carga_futbolistas()
 {
-        int n_futbolistas, i, j;
+        int i, j;
         char aux[21], letra;
         f_futbolista=fopen("futbolistas.txt", "r");
         if(f_futbolista==NULL)
@@ -228,9 +222,9 @@ void carga_futbolistas()
         }
         else
         {
-                n_futbolistas=contar_lineas(f_futbolista);
+                lineas_futbolistas=contar_lineas(f_futbolista);
                 rewind(f_futbolista);
-                total_futbolistas=(futbolista *) malloc (n_futbolistas*sizeof(futbolista));
+                total_futbolistas=(futbolista *) malloc (lineas_futbolistas*sizeof(futbolista));
                 if(total_futbolistas==NULL)
                 {
                         printf("error al reservar espacio de memoria para futbolistas \n");
@@ -285,6 +279,17 @@ void carga_futbolistas()
                                 total_futbolistas[i].precio_futbolista=atoi(aux);
                                 limpiar(aux);
                                 letra='0';
+                                for(j=0; !feof(f_futbolista) && letra!='-'; j++)
+                                {
+                                        letra=fgetc(f_futbolista);
+                                        if(letra!='-')
+                                        {
+                                                aux[j]=letra;
+                                        }
+                                }
+                                total_futbolistas[i].valoracion=atoi(aux);
+                                limpiar(aux);
+                                letra='0';
                                 for(j=0; !feof(f_futbolista) && letra!='\n'; j++)
                                 {
                                         letra=fgetc(f_futbolista);
@@ -293,7 +298,7 @@ void carga_futbolistas()
                                                 aux[j]=letra;
                                         }
                                 }
-                                total_futbolistas[i].valoracion=atoi(aux);
+                                total_futbolistas[i].valoracion_total=atoi(aux);
                         }
                 }
         }
@@ -302,7 +307,7 @@ void carga_futbolistas()
 
 void carga_plantillas()
 {
-        int i, j, numero_plantillas;
+        int i, j, k;
         char cad_aux[31], letra;
         f_plantillas=fopen("Plantillas.txt", "r");
         if(f_plantillas==NULL)
@@ -311,9 +316,9 @@ void carga_plantillas()
         }
         else
         {
-                numero_plantillas=contar_lineas(f_plantillas);
+                lineas_plantillas=contar_lineas(f_plantillas);
                 rewind(f_plantillas);
-                total_plantillas=(plantilla *) malloc(numero_plantillas*sizeof(plantilla));
+                total_plantillas=(plantilla *) malloc(lineas_plantillas*sizeof(plantilla));
                 if(total_plantillas==NULL)
                 {
                         printf("error al reservar espacio de memoria para plantillas. \n");
@@ -377,59 +382,133 @@ void carga_plantillas()
                                         }
                                 }
                                 total_plantillas[i].puntuacion_plantilla=atoi(cad_aux);
+                                limpiar(cad_aux);
+                                letra='0';
+                                for(j=0; !feof(f_plantillas) && letra!='\n'; j++)
+                                {
+                                      for(k=0;!feof(f_plantillas) && letra!='-';k++)
+                                      {
+                                        letra=fgetc(f_plantillas);
+                                        if(letra!='-')
+                                        {
+                                          cad_aux[k]=letra;
+                                        }
 
+                                      }
+                                      total_plantillas[i].jugadores[j]=atoi(cad_aux);
+                                      limpiar(cad_aux);
+                                      letra='0';
+                                }
                         }
                 }
         }
         fclose(f_plantillas);
 }
 
-void carga_jugadores_plantillas()
+void descarga_configuracion()
 {
-        int i, j, numero_jug_plantillas;
-        char letra, cad_aux[4];
-        f_jugadores_plantilla=fopen("jugadores_plantilla.txt", "r");
-        if(f_jugadores_plantilla==NULL)
+  f_configuracion=fopen("configuracion.txt","w");
+  if(f_configuracion==NULL)
+{
+  printf("error al abrir fichero \n");
+}
+  else
+{
+  int i=-1;
+  do{
+    i++;
+    fprintf(f_configuracion,"$d-$s-$d\n",toda_config[i].identificador_configuracion, toda_config[i].campo_configuracion, toda_config[i].valor_campo);
+  }while(toda_config[i+1].identificador_configuracion==toda_config[i].identificador_configuracion+1);
+}
+  fclose(f_configuracion);
+  free(toda_config);
+}
+
+void descarga_usuario()
+{
+f_usuario=fopen("usuarios.txt","w");
+if(f_usuario==NULL)
+{
+  printf("error al abrir fichero \n");
+}
+  else
+  {
+    int i=-1;
+    do{
+      i++;
+      fprintf(f_usuario, "$d-$s-$d-$s-$s\n", total_usuarios[i].identificador_usuario, total_usuarios[i].nombre_usuario, total_usuarios[i].perfil_usuario, total_usuarios[i].user, total_usuarios[i].contrasena);
+    }while(total_usuarios[i+1].identificador_usuario==total_usuarios[i].identificador_usuario+1);
+  }
+  fclose(f_usuario);
+  free(total_usuarios);
+}
+
+void descarga_futbolistas()
+{
+  f_futbolista=fopen("futbolistas.txt", "w");
+  if(f_futbolista==NULL)
+{
+  printf("error al abrir fichero \n");
+}
+  else
+{
+  int i=-1;
+  do{
+    i++;
+    fprintf(f_futbolista,"%d-%d-%s-%d-%d-%d\n",total_futbolistas[i].identificador_futbolista, total_futbolistas[i].identificador_equipo_pertenece, total_futbolistas[i].nombre_futbolista, total_futbolistas[i].precio_futbolista, total_futbolistas[i].valoracion, total_futbolistas[i].valoracion_total);
+  }while(total_futbolistas[i+1].identificador_futbolista==total_futbolistas[i].identificador_futbolista+1);
+}
+fclose(f_futbolista);
+free(total_futbolistas);
+}
+
+void descarga_equipos()
+{
+  f_equipos=fopen("equipos.txt", "w");
+  if(f_equipos==NULL)
+  {
+    printf("error al abrir fichero \n");
+  }
+    else
+    {
+      int i=-1;
+      do{
+        i++;
+        fprintf(f_equipos,"%d-%s\n", total_equipos[i].identificador_equipo, total_equipos[i].nombre_equipo);
+      }while(total_equipos[i+1].identificador_equipo==total_equipos[i].identificador_equipo+1);
+    }
+    fclose(f_equipos);
+    free(total_equipos);
+}
+
+
+void descarga_plantillas()
+{
+  f_plantillas=fopen("Plantillas.txt", "w");
+  if(f_plantillas==NULL)
+  {
+    printf("error al abrir fichero \n");
+  }
+  else
+  {
+    int i=-1, j, cuenta=0;
+    do{
+      i++;
+      fprintf(f_plantillas,"$d-%d-%s-%d-%d",total_plantillas[i].identificador_prop_plantilla, total_plantillas[i].identificador_plantilla, total_plantillas[i].nombre_plantilla, total_plantillas[i].presupuesto, total_plantillas[i].puntuacion_plantilla);
+      for(j=0;j<11;j++)
+      {
+        fprintf(f_plantillas,"$d", total_plantillas[i].jugadores[j]);
+        if(cuenta<10)
         {
-                printf("error al abrir fichero de jugadores de plantillas. \n");
+          fprintf(f_plantillas, "-");
         }
         else
         {
-                numero_jug_plantillas=contar_lineas(f_jugadores_plantilla);
-                rewind(f_jugadores_plantilla);
-                total_jugad_plant=(jugadores_plantilla *) malloc(numero_jug_plantillas*sizeof(jugadores_plantilla));
-                if(total_jugad_plant==NULL)
-                {
-                        printf("error al reservar memoria para los jugadores de las plantillas. \n");
-                }
-                else
-                {
-                        for(i=0; !feof(f_jugadores_plantilla); i++)
-                        {
-                                limpiar(cad_aux);
-                                letra='0';
-                                for(j=0; !feof(f_jugadores_plantilla) && letra!='-'; j++)
-                                {
-                                        letra=fgetc(f_jugadores_plantilla);
-                                        if(letra!='-')
-                                        {
-                                                cad_aux[j]=letra;
-                                        }
-                                }
-                                total_jugad_plant[i].identificador_jugador=atoi(cad_aux);
-                                limpiar(cad_aux);
-                                letra='0';
-                                for(j=0; !feof(f_jugadores_plantilla) && letra!='\n'; j++)
-                                {
-                                        letra=fgetc(f_jugadores_plantilla);
-                                        if(letra!='\n')
-                                        {
-                                                cad_aux[j]=letra;
-                                        }
-                                }
-                                total_jugad_plant[i].identificador_plantilla=atoi(cad_aux);
-                        }
-                }
+          fprintf(f_plantillas,"\n");
         }
-        fclose(f_jugadores_plantilla);
+      }
+    }while(total_plantillas[i+1].identificador_plantilla==total_plantillas[i].identificador_plantilla+1);
+  }
+  fclose(f_plantillas);
+  free(total_equipos);
 }

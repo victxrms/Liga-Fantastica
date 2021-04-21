819cb0c0e4f16909761a3f2687263193dcdb3f98
AlbertoValderas <albertovg2512@gmail.com>
2021-04-22 00:33:52 +0200

    avance

--- equipo.c
+++ equipo.c
@@ -9,27 +9,8 @@
 
 #define presupuesto 1000
 
-int ident_plantillas();
 int numero_plantillas(int );
 void equipo(int );
-
-int ident_plantillas()
-{
-    int i, jug, ident_plant, ident_usuario;
-    char nombre[21];
-
-    plantillas=fopen("Plantillas.txt","r");
-
-    while(fscanf("%i %s %i", &ident_plant, &nombre, &ident_usuario)==3)
-    {
-        for(i=0; i<11; i++)
-            fscanf("%i", &jug)
-    }
-
-    fclose(plantillas);
-
-    return ident_plant;
-}
 
 int numero_plantillas(int x)
 {
@@ -45,7 +26,7 @@ void equipo(int x)
 {
     FILE *fichero;
 
-	int i=1, i2=0, i3=0, i4=0, n=0, j[11], salida=0, reset2, codigo, x, y;
+	int i=1, i2=0, i3=0, i4=0, n=0, j[11], salida=0, reset2, codigo, y;
 	float precio, valor=0, diferencia;
 	char user[9], menu1, c, reset;
 
@@ -129,80 +110,68 @@ void equipo(int x)
                         do
                         {
                             printf("Introduzca el codigo de su portero (numeros 1, 5, 9 ,13...) --> ");
-                            scanf("%i", &j[0]);
-                            if(j[0]%4!=1||j[0]>80)
+                            scanf("%i", &total_plantillas.jugadores[0]);
+                            if(total_plantillas.jugadores[0]%4!=1||total_plantillas.jugadores[0]>80)
                                 printf("Codigo no existente o no es portero.\n");
-                        }while(j[0]%4!=1||j[0]>80);
+                        }while(total_plantillas.jugadores[0]%4!=1||total_plantillas.jugadores[0]>80);
 
                         do
                         {
                             printf("Introduzca el codigo de sus cuatro defensas (separados por espacios) (numeros 2, 6, 10 ,14...) --> ");
                             scanf("%i %i %i %i", &j[1], &j[2], &j[3], &j[4]);
-                            if(j[1]%4!=2||j[2]%4!=2||j[3]%4!=2||j[4]%4!=2||j[1]>80||j[2]>80||j[3]>80||j[4]>80)
+                            if(total_plantillas.jugadores[1]%4!=2||total_plantillas.jugadores[2]%4!=2||total_plantillas.jugadores[3]%4!=2||total_plantillas.jugadores[4]%4!=2||total_plantillas.jugadores[1]>80||total_plantillas.jugadores[2]>80||total_plantillas.jugadores[3]>80||total_plantillas.jugadores[4]>80)
                                 printf("Codigo/s no existente/s o no es/son defensa/s.\n");
-                        }while(j[1]%4!=2||j[2]%4!=2||j[3]%4!=2||j[4]%4!=2||j[1]>80||j[2]>80||j[3]>80||j[4]>80);
+                        }while(total_plantillas.jugadores[1]%4!=2||total_plantillas.jugadores[2]%4!=2||total_plantillas.jugadores[3]%4!=2||total_plantillas.jugadores[4]%4!=2||total_plantillas.jugadores[1]>80||total_plantillas.jugadores[2]>80||total_plantillas.jugadores[3]>80||total_plantillas.jugadores[4]>80);
 
                         do
                         {
                             printf("Introduzca el codigo de sus tres centrocampistas (separados por espacios) (numeros 3, 7, 11 ,15...) --> ");
                             scanf("%i %i %i", &j[5], &j[6], &j[7]);
-                            if(j[5]%4!=3||j[6]%4!=3||j[7]%4!=3||j[5]>80||j[6]>80||j[7]>80)
+                            if(total_plantillas.jugadores[5]%4!=3||total_plantillas.jugadores[6]%4!=3||total_plantillas.jugadores[7]%4!=3||total_plantillas.jugadores[5]>80||total_plantillas.jugadores[6]>80||total_plantillas.jugadores[7]>80)
                                 printf("Codigo/s no existente/s o no es/son centrocampista/s.\n");
-                        }while(j[5]%4!=3||j[6]%4!=3||j[7]%4!=3||j[5]>80||j[6]>80||j[7]>80);
+                        }while(total_plantillas.jugadores[5]%4!=3||total_plantillas.jugadores[6]%4!=3||total_plantillas.jugadores[7]%4!=3||total_plantillas.jugadores[5]>80||total_plantillas.jugadores[6]>80||total_plantillas.jugadores[7]>80);
 
                         do
                         {
                             printf("Introduzca el codigo de sus tres delanteros (separados por espacios) (numeros 4, 8, 12 ,16...) --> ");
                             scanf("%i %i %i", &j[8], &j[9], &j[10]);
-                            if(j[8]%4!=0||j[9]%4!=0||j[10]%4!=0||j[8]>80||j[9]>80||j[10]>80)
+                            if(total_plantillas.jugadores[8]%4!=0||total_plantillas.jugadores[9]%4!=0||total_plantillas.jugadores[10]%4!=0||total_plantillas.jugadores[8]>80||total_plantillas.jugadores[9]>80||total_plantillas.jugadores[10]>80)
                                 printf("Codigo/s no existente/s o no es/son delantero/s.\n");
-                        }while(j[8]%4!=0||j[9]%4!=0||j[10]%4!=0||j[8]>80||j[9]>80||j[10]>80);
+                        }while(total_plantillas.jugadores[8]%4!=0||total_plantillas.jugadores[9]%4!=0||total_plantillas.jugadores[10]%4!=0||total_plantillas.jugadores[8]>80||total_plantillas.jugadores[9]>80||total_plantillas.jugadores[10]>80);
 
                         printf("\n");
 
                         for(i=0; i<11; i++)
-                            printf("%i ", j[i]);
+                            printf("%i ", total_plantillas.jugadores[i]);
 
                         printf("\n\nSon correctos estos codigos? [s/n] --> ");
                         fflush(stdin);
                         scanf("%c", &reset);
                     }while(reset!='s'&&reset!='S');
 
-                    jugadores=fopen("jugadores2.txt","r");
-
                     i=0;
 
-                    while((fscanf(jugadores, "%i %f ", &codigo, &precio)==2)&&i<11)
+                    for(i=0;i<11;i++)
+                        total_plantillas.presupuesto+=total_futbolistas[total_plantillas.jugadores[i]].precio_futbolista;
+
+                    if(total_plantillas.presupuesto>toda_config[1].valor_campo)
                     {
-                        if(j[i]==codigo)
-                        {
-                            valor+=precio;
-                            rewind(jugadores);
-                            i++;
-                        }
+                        printf("Su plantilla se pasa del presupuesto por %i millones. Por favor, vuelva a elegir sus jugadores.", total_plantillas.presupuesto-toda_config[1].valor_campo);
+                        reset2=1;
                     }
-
-                    fclose(jugadores);
-
-                    diferencia=presupuesto-valor;
-
-                    if(diferencia>=0) //variable presupuesto
+                    else if(total_plantillas.presupuesto<toda_config[0].valor_campo)
                     {
-                        plantillas=fopen("plantillas.txt","a");
-                        for(i=0; i<10; i++)
-                            fprintf(plantillas, "%i ", j[i]);
-                        fprintf(plantillas, "%i\n", j[10]);
-                        fclose(plantillas);
-                        printf("Su plantilla ha sido creada correctamente.");
-                        reset2=0;
+                        printf("Su plantilla esta por debajo del presupuesto por %i millones. Por favor, vuelva a elegir sus jugadores.", toda_config[0].valor_campo-total_plantillas.presupuesto);
+                        reset2=1;
                     }
                     else
                     {
-                        printf("Su plantilla se pasa del presupuesto por %.1f millones. Por favor, vuelva a elegir sus jugadores.", diferencia*-1);
-                        reset2=1;
+                        for(i=0;i<11;i++)
+                            total_plantillas.puntuacion_plantilla+=total_futbolistas[total_plantillas.jugadores[i]].valoracion_total;
+                        printf("Su plantilla ha sido creada correctamente.");
+                        reset2=0;
                     }
                 }while(reset2!=0);
-
             }
         break;
     }
